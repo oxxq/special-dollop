27eaea38ce7b37e0b4872e774fe577f18c8adbaf
Mastaaa1987 <125365308+Mastaaa1987@users.noreply.github.com>
2023-02-23 15:41:45 +0100

    Update m3u8.php

--- lighttpd/www/m3u8.php
+++ lighttpd/www/m3u8.php
@@ -1,4 +1,4 @@
 <?php
-    $e = shell_exec("/data/data/com.termux/files/usr/bin/python2 /data/data/com.termux/files/home/lighttpd/www/playlist.py m3u8");
+    $e = shell_exec("/usr/bin/python2 playlist.py m3u8");
     echo "$e";
 ?>
