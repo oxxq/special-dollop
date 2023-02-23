f3c045b8600ea20f9048612a76f3e5c2eae0f221
Mastaaa1987 <125365308+Mastaaa1987@users.noreply.github.com>
2023-02-23 15:45:55 +0100

    Update sig.php

--- lighttpd/www/sig.php
+++ lighttpd/www/sig.php
@@ -1,6 +1,6 @@
 <?php
     $db=new SQLite3('playlist.db');
-    $nsig = shell_exec("/data/data/com.termux/files/usr/bin/python2 /data/data/com.termux/files/home/lighttpd/www/playlist.py sig2");
+    $nsig = shell_exec("python2 playlist.py sig2");
     if ($nsig) {
         $query = $db->exec('DELETE FROM sig');
         $query = $db->exec("INSERT INTO sig (sig,time) VALUES ('".$nsig."', '".time()."')");
