effad5d5b0374f1f96ef0ef308106975923d2ce4
Mastaaa1987 <125365308+Mastaaa1987@users.noreply.github.com>
2023-02-23 15:46:15 +0100

    Update playlist2.php

--- lighttpd/www/playlist2.php
+++ lighttpd/www/playlist2.php
@@ -7,7 +7,7 @@
         $stime = $row['time'];
         $now = time();
         if($now > $stime + 300) {
-            $nsig = shell_exec("/usr/bin/python2 playlist.py sig2");
+            $nsig = shell_exec("python2 playlist.py sig2");
             if ($nsig) {
                 $query = $db->exec('DELETE FROM sig');
                 $query = $db->exec("INSERT INTO sig (sig,time) VALUES ('".$nsig."', '".time()."')");
