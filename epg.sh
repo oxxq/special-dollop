61f777ebb2a1ac542f57ffdf92ee76f0651753ea
Mastaaa1987 <125365308+Mastaaa1987@users.noreply.github.com>
2023-02-23 15:39:03 +0100

    Change File for Unix support ....

--- epg.sh
+++ epg.sh
@@ -1,24 +1,22 @@
-#!/data/data/com.termux/files/usr/bin/bash
+#!/bin/bash
 
 if [ "$1" == "epg" ] || [ "$1" == "" ]; then
     ro="/data/data/com.termux/files/home";
 
-    db="$ro/lighttpd/www/playlist.db";
+    db="/var/www/playlist.db";
     if ! [ -e "$db" ]; then echo "Error! playlist.db not found!"; exit 1; fi;
     xmltv=`which tvspielfilm2xmltv`;
     if ! [ -e "$xmltv" ]; then
-        xmltv="$ro/.local/bin/tvspielfilm2xmltv";
+        xmltv="$HOME/.local/bin/tvspielfilm2xmltv";
         if ! [ -e "$xmltv" ]; then
-            xmltv="/data/data/com.termux/files/usr/lib/python2.7/site-packages/tvspielfilm2xmltv.py";
+            xmltv="/usr/lib/python2.7/site-packages/tvspielfilm2xmltv.py";
             if ! [ -e "$xmltv" ]; then echo "Error! tvspielfilm2xmltv not found!"; exit 1; fi;
         fi;
     fi;
-    ini="$ro/tvspielfilm2xmltv.ini";
+    ini="tvspielfilm2xmltv.ini";
     if ! [ -e "$ini" ]; then echo "Error! tvspielfilm2xmltv.ini not found!"; exit 1; fi;
-    con="$ro/tvspielfilm2xmltv.control";
+    con="tvspielfilm2xmltv.control";
     if ! [ -e "$ini" ]; then echo "Error! tvspielfilm2xmltv.control not found!"; exit 1; fi;
-
-    cd $ro;
 
     epgfile=`cat $ini | grep destination_file | sed -e 's/.* = //g'`;
     confile=`cat $ini | grep control_file | sed -e 's/.* = //g'`;
@@ -36,11 +34,9 @@ if [ "$1" == "epg" ] || [ "$1" == "" ]; then
 "Sony Channel" "Spiegel Geschichte" "Spiegel TV Wissen" \
 "SUPER RTL" "SWR/SR" "Syfy" "tagesschau24" "Tele 5" "TLC" "TNT Comedy" "TNT Film" "TNT Serie" "TOGGO plus" "Universal Channel HD" "VOX" "VOXup" "WDR" "ZDF" "ZDFinfo" "ZDFneo";
     if ! [ -e "$epgfile" ]; then echo "Error! epg.xml not found!"; exit 1; fi;
-    gzip epg.xml;
-    if [ -e "epg.xml.gz" ]; then
-        if [ -e "lighttpd/www/epg.xml.gz" ]; then rm lighttpd/www/epg.xml.gz; fi;
-        mv epg.xml.gz lighttpd/www/;
-        echo "epg.xml.gz successful created @ $ro/lighttpd/www";
+    gzip $epgfile;
+    if [ -e "${epgfile}.gz" ]; then
+        echo "epg.xml.gz successful created @ $(dirname $epgfile)";
         exit 0;
     else echo "Error! creating epg.xml.gz!"; exit 1; fi;
 else
